699937baec5050672a3ed5fc7d572b4af19305a4
arbaj <khanarbaj45@gmail.com>
2021-03-08 18:22:35 +0530

    Feat : Added features

--- f1.py
+++
@@ -1,5 +0,0 @@
-import gftools.fonts_public_pb2 as fonts_pb2
-from fontTools.ttLib import TTFont
-
-ttf = TTFont('a.ttf', lazy=True)
-ttf.saveXML("a.xml")
--- main.py
+++ main.py
@@ -1,11 +1,45 @@
 from fontTools.ttLib import TTFont
 
-chars = []
-with TTFont('a.ttf', 0, ignoreDecompileErrors=True) as ttf:
-    for x in ttf["cmap"].tables:
-        for (code, _) in x.cmap.items():
-            chars.append(chr(code))
-for s in chars:
+
+def emoji(file):
+    chars = []
+    with TTFont(file, 0, ignoreDecompileErrors=True) as ttf:
+        for x in ttf["cmap"].tables:
+            [chars.append(chr(code)) for (code, _) in x.cmap.items() if chr(code) not in chars]
+    return chars
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#     print(chars)
+#     print(f"length is {len(chars)}")
+# file = "Recursive_VF_1.053.ttf"
+# emoji(file)
+# for s in chars:
   # print(f"{s} \t {s.encode('unicode_escape')}")
   # print(f"{s} \t {s.encode('utf-8')}")
-  print(f"{s} \t {s.encode('unicode_escape')}")
+  # print(f"{s} \t {s.encode('unicode_escape')}")
